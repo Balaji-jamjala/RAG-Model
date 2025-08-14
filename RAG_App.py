cbad35821cb291c8c55ef8ae1591cfd33e4570ee
Balaji-jamjala <jamjalabalaji555@gmail.com>
2025-08-14 11:34:42 +0530

    Update RAG_App.py

--- RAG_App.py
+++ RAG_App.py
@@ -96,9 +96,9 @@ if prompt_input:=st.chat_input("Ask a question about your documents..."):
       
       with st.spinner("Thinking..."):
         
-        document_chain=create_stuff_documents_chain(tlm, prompt)
+        document_chain= create_stuff_documents_chain(llm, prompt)
 
-        retriever = st.session_state.vector.as_retrieverli
+        retriever = st.session_state.vector.as_retriever()
         
         retrieval_chain = create_retrieval_chain(retriever, document_chain)
         
